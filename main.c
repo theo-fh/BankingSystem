b2b886d93d10c7ae1aeb6d406e71492bc8722374
Theo Hoepfner <theo.fr.hoepfner@gmail.com>
2026-03-27 16:14:47 +0100

    newAccount() hinzugefügt, Memory Leak Warnung

--- main.c
+++ main.c
@@ -1,26 +1,127 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define DATEI ("../accounts")
+#define ZEILENLAENGE (int zeilenlaenge = 62;)
 
 struct Konto {
     int guthaben; //in ct
     char inhaber[40];
-} konten[];
+};
 
-int auslesen() {
-    FILE* accounts = fopen(DATEI, "r");
+void clearInput();
+int anzahlKonten(FILE*); //Nicht mehr richtig, sobald newAccount() benutzt wurde!!!
+void auslesen(FILE*, struct Konto*, int);
+int writeToFile();
+void newAccount(struct Konto*, int*);
+int withdraw();
+int deposit();
+int transfer();
+
+int main() {
+
+    FILE* accounts = fopen(DATEI, "r+");
+    if (accounts == NULL) {
+        printf("Fehler beim lesen der Datei.");
+        exit(1);
+    }
+
+    int anzahl = anzahlKonten(accounts);
+    int *anzahlPtr = &anzahl;
+    struct Konto *konten = (struct Konto*) malloc(anzahl*sizeof(struct Konto)); //Gibt Warnung über Memory Leak, hab ich mich aber eigentlich drum gekümmert...
+
+    auslesen(accounts, konten, anzahl);
+
+    char operation = 'n';
+    if (operation == 'n') {
+
+
+        struct Konto *tmpAos = realloc(konten, anzahl*sizeof(struct Konto) + sizeof(struct Konto)); // Platz für weiteren Eintrag
+        if (konten == NULL) {
+            printf("Fehler bei Speicherzuweisung.");
+            free(tmpAos);
+            exit(1);
+        }
+        else {
+
+            konten = tmpAos;
+
+            newAccount(konten, anzahlPtr);
+
+            printf("\nKonto erfolgreich erstellt.");
+        }
+    } //neues Konto anlegen
+
+    free(konten);
+    fclose(accounts);
+
+    return 0;
+}
+
+
+
+//FUNKTIONEN
+
+void clearInput() {
+    int c;
+    while ((c = getchar()) != EOF && c != '\n') {}
+}
+
+int anzahlKonten(FILE* datei) {
 
     //Menge der char in der File lesen (bis EOF)
+    char c;
+    int i = 0;
+    while ((c = fgetc(datei)) != EOF) {
+        i++;
+    }
 
     //daraus Anzahl der Konten (--> feste Zeilenlänge)
+    int zeilenlänge = 62;
+    int anzahlKonten = i/zeilenlänge;
 
-    //konten = malloc(anzahl*sizeof(struct Konto)) für das AOS
+    return anzahlKonten + 1;
+}
 
-    //Alle Daten in konten[i].guthaben und -.inhaber schreiben
+void auslesen(FILE* datei, struct Konto *aos, int anzahl) {
+    int zeilenlaenge = 62;
+    fseek(datei, 0, SEEK_SET);
+    for (int kontoNr = 0; kontoNr < anzahl; kontoNr++) {
+        fseek(datei, zeilenlaenge * kontoNr, SEEK_SET);
+        char guthabenStr[30] = {0};
+        char inhaberStr[40] = {0};
+        int n = 0;
+        char c;
+        while ((c = fgetc(datei)) != ' ') {
+            guthabenStr[n] = c;
+            n++;
+        } //Guthaben des Kontos lesen
+        guthabenStr[n] = '\0'; //String kürzen, damit nur "Integer" und '\0' enthalten sind
+        int stellen = n-1;
 
-    //evtl. return Anzahl der Konten
+        int guthaben = 0;
+        for (int i = stellen; i >= 0; i--) {
+            guthaben += ((int) guthabenStr[i] - 48)*pow(10, stellen - i);
+        }
+        (aos + kontoNr)->guthaben = guthaben;
 
-    fclose(accounts);
+        fseek(datei, (long int) zeilenlaenge * kontoNr + 20, SEEK_SET);
+        n = 0;
+        while ((c = fgetc(datei)) != '/') {
+            inhaberStr[n] = c;
+            n++;
+        } //Inhaber des Kontos lesen
+
+        for (int i = 39; i >= 0; i--) {
+            if (inhaberStr[i] != ' ') {
+                inhaberStr[i+1] = '\0';
+                break;
+            }
+        }
+
+        strcpy((aos + kontoNr)->inhaber, inhaberStr);
+    }
 }
 
 int writeToFile() {
@@ -50,17 +151,39 @@ int writeToFile() {
     --> funktioniert noch nicht ganz, aber hilfreich für das schreiben von AOS in Datei
     */
 
+
     fclose(accounts);
 }
 
-int newAccount(struct Konto *aos, char inhaber[40], int startguthaben) {
+void newAccount(struct Konto *aos, int* anzahlPtr) {
+    //Anzahl der Konten intern erhöhen
+    int anzahl = *(anzahlPtr) + 1;
 
-    //irgendwoher die Anzahl aller Konten vor dieser Funktion herausfinden
+    char inhaberNeu[40];
+    int startguthaben;
+    printf("NEUES KONTO \nInhaber:");
+    fgets(inhaberNeu, 40, stdin);
 
-    //realloc(aos, anzahl*sizeof(struct Konto) + sizeof(struct Konto)) --> Platz für weiteren eintrag
 
-    //letzter (neuer) Eintrag: aos[letzter].inhaber = inhaber[40]; -.guthaben = startguthaben
+    // Newline aus dem Input entfernen
+    for (int i = 0; i < 40; i++) {
+        if (inhaberNeu[i] == '\n') {
+            inhaberNeu[i] = '\0';
+            break;
+        }
+    }
 
+
+    printf("Startguthaben in Cent: ");
+    scanf(" %d", &startguthaben);
+    clearInput();
+
+    //Angaben in AOS übernehmen
+    strcpy((aos + anzahl - 1)->inhaber, inhaberNeu);
+    (aos + anzahl - 1)->guthaben = startguthaben;
+
+    //Anzahl d. Konten global erhöhen
+    *anzahlPtr += 1;
 }
 
 int withdraw() {
@@ -74,16 +197,3 @@ int deposit() {
 int transfer() {
 
 } //Kombination aus withdraw und deposit
-
-int main() {
-
-    char operation;
-    printf("Operation: ");
-    scanf(" %c", &operation);
-
-    if (operation == 'w')
-        withdraw();
-
-
-    return 0;
-}
